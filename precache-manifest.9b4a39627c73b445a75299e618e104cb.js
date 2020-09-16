abc69f1553efc6ab2ea12b0af4b59ddd20e8ec1b
thePmSquare <impmmmm@gmail.com>
2020-09-16 18:01:59 +0530

    Updates

---
+++ precache-manifest.9b4a39627c73b445a75299e618e104cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef3a5b0c00b64777dc748d69ac8609a9",
+    "url": "/ttt-game-react/index.html"
+  },
+  {
+    "revision": "62df0e3571cad2aa8224",
+    "url": "/ttt-game-react/static/css/main.56d80a55.chunk.css"
+  },
+  {
+    "revision": "4ff48e399a7942385b07",
+    "url": "/ttt-game-react/static/js/2.dd220570.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/ttt-game-react/static/js/2.dd220570.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62df0e3571cad2aa8224",
+    "url": "/ttt-game-react/static/js/main.9867e280.chunk.js"
+  },
+  {
+    "revision": "a9145d22bc3e3c5a62a9",
+    "url": "/ttt-game-react/static/js/runtime-main.8f3afb72.js"
+  }
+]);
